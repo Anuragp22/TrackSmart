82a349ed4ad135f1840c367849065dfc59c795fe
shauryaraj-kgp <146989609+shauryaraj-kgp@users.noreply.github.com>
2025-02-25 00:23:29 +0530

    Structured the FrontEnd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
+import Login from "./pages/Login.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
-
-
   return (
-    <>
-      <div className='text-red-400'>TrackSmart</div>
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+    return <h1>Welcome to Expense Tracker</h1>;
+}
+
+export default Dashboard;
